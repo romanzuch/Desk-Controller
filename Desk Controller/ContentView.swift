0d09689ce26c74bb80008c9fe41072cde782d558
Roman <roman.zuchowski@gmail.com>
2023-10-01 18:16:15 +0200

    Initial Commit

---
+++ Desk Controller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Desk Controller
+//
+//  Created by Roman Zuchowski on 01.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Desk Controller/Desk_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desk_ControllerApp.swift
+//  Desk Controller
+//
+//  Created by Roman Zuchowski on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Desk_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
