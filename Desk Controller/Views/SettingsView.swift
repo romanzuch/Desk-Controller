2b0372d32756b0e3e9bd7cde3e3b5ae3a0743859
Roman <roman.zuchowski@gmail.com>
2023-10-07 16:16:19 +0200

    this will implement refactored buttons

--- Desk Controller/Views/SettingsView.swift
+++ Desk Controller/Views/SettingsView.swift
@@ -11,29 +11,42 @@ struct SettingsView: View {
     
     @EnvironmentObject var btDelegate: CBManagerDelegate
     @EnvironmentObject var settings: Settings
+    @State private var showSettings: Bool = false
     
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text("Einstellungen")
-                .font(.headline)
-                .fontWeight(.bold)
-            // Set the table height preferences
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Voreinstellung Tischhöhe")
-                SettingsSlider(title: "Hoch", value: $settings.deskSettingHigh, range: 85.0...120.0, cornerRadius: 16, material: .ultraThinMaterial)
-                SettingsSlider(title: "Mittel", value: $settings.deskSettingMid, range: 65.0...85.0, cornerRadius: 16, material: .ultraThinMaterial)
-                SettingsSlider(title: "Niedrig", value: $settings.deskSettingLow, range: 40.0...65.0, cornerRadius: 16, material: .ultraThinMaterial)
-            }
-            // Current table selection
-            Text("Aktuelle Auswahl: \(self.btDelegate.peripheral?.name ?? "")")
-            // Select a table
-            Picker(selection: $settings.savedPeripheral) {
-                ForEach(self.btDelegate.peripheralsAdditional, id: \.name) { peripheral in
-                    Text(peripheral.name ?? "")
+            HStack {
+                Text("Einstellungen")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                Spacer()
+                Button {
+                    showSettings.toggle()
+                } label: {
+                    Image(systemName: showSettings == true ? "chevron.up" : "chevron.down")
                 }
-            } label: {
-                Text("Tisch auswählen")
+                .buttonStyle(.plain)
+            }
+            Spacer()
+            if showSettings == true {
+                // Set the table height preferences
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Voreinstellung Tischhöhe")
+                    SettingsSlider(title: "Hoch", value: $settings.deskSettingHigh, range: 85.0...120.0, cornerRadius: 16, material: .ultraThinMaterial)
+                    SettingsSlider(title: "Mittel", value: $settings.deskSettingMid, range: 65.0...85.0, cornerRadius: 16, material: .ultraThinMaterial)
+                    SettingsSlider(title: "Niedrig", value: $settings.deskSettingLow, range: 40.0...65.0, cornerRadius: 16, material: .ultraThinMaterial)
+                }
+                // Current table selection
+                Text("Aktuelle Auswahl: \(self.btDelegate.peripheral?.name ?? "")")
+                // Select a table
+//                Picker(selection: $settings.savedPeripheral) {
+//                    ForEach(self.btDelegate.peripheralsAdditional, id: \.name) { peripheral in
+//                        Text(peripheral.name ?? "")
+//                    }
+//                } label: {
+//                    Text("Tisch auswählen")
+//                }
             }
         }
     }
